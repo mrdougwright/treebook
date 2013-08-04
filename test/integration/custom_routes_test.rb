21f479344c101f23bbecc10bb413c67c4c1d701a
Doug Wright <mrdougwright@gmail.com>
2013-08-03 23:13:56 -0700

    Update profile page and add scoped status updates

--- test/integration/custom_routes_test.rb
+++ test/integration/custom_routes_test.rb
@@ -17,4 +17,8 @@ class CustomRoutesTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
   
+  test "that a profile page works" do
+    get '/Dougbo'
+    assert_response :success
+  end
 end
