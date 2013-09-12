cb7906384d85c2c2474044d192cc87e505909525
Doug Wright <mrdougwright@gmail.com>
2013-09-12 15:23:05 -0700

    Add user friendships and associated tests

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  should have_many(:user_friendships)
+  should have_many(:friends)
+
   test "a user should enter a first name" do
     user = User.new
     assert !user.save
@@ -43,4 +46,19 @@ class UserTest < ActiveSupport::TestCase
     user.profile_name = 'dougwright1'
     assert user.valid?
   end
+
+  test "that no error is raised when trying to access a friend list" do
+    assert_nothing_raised do
+      users(:doug).friends
+    end
+  end
+
+  test "that creating friendships on a user works" do
+    users(:doug).friends << users(:mike)
+    users(:doug).friends.reload
+    assert users(:doug).friends.include?(users(:mike))
+  end
+
 end
+
+
